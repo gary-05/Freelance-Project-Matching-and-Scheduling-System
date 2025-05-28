3bc0a8153325a8a06d9b6e35eca8c9f3390a44e2
Garima <garimasingh0529@gmail.com>
2025-05-28 06:40:40 +0530

    Update Project.java

--- Project.java
+++ Project.java
@@ -1,6 +1,8 @@
 package mySystem;
 
+import java.util.Arrays;
 public class Project {
+    String id;
     String title;
     String description;
     String[] requiredSkills;
@@ -8,7 +10,8 @@ public class Project {
     int end;
     int pay;
 
-    public Project(String title, String description, String[] requiredSkills, int start, int end, int pay) {
+    public Project(String id, String title, String description, String[] requiredSkills, int start, int end, int pay) {
+        this.id = id;
         this.title = title;
         this.description = description;
         this.requiredSkills = requiredSkills;
@@ -19,6 +22,14 @@ public class Project {
 
     @Override
     public String toString() {
-        return title + " (Start: " + start + ", End: " + end + ", Pay: Rs." + pay + ")";
+    	String startInt=String.valueOf(start);
+    	String d1=startInt.substring(0,2);
+    	String m1=startInt.substring(2,4);
+    	String y1=startInt.substring(4);
+    	String endInt=String.valueOf(end);
+    	String d2=endInt.substring(0,2);
+    	String m2=endInt.substring(2,4);
+    	String y2=endInt.substring(4);
+        return id + ": " +"\n\t"+ title +"\n\t"+ description+"\n\t"+Arrays.toString(requiredSkills)+"\n\t" +"(Start: " + d1+"-"+m1+"-"+y1 + ", End: " + d2+"-"+m2+"-"+y2 + ", Pay: Rs." + pay + ")";
     }
 }
